b821a99b0a890b4d9eb24384b6e8984dcb77be42
facundo2gonzalez2 <facundo2gonzalez2@gmail.com>
2023-04-09 19:46:25 -0300

    ej 1  facu

--- tp1/ejercicio1_facu.cpp
+++ tp1/ejercicio1_facu.cpp
@@ -74,51 +74,10 @@ vector<vector<int>> cuadradoMagicoKesimo(int n, int i, int j, vector<vector<int>
     vector<vector<int>> error(1,vector<int>(1,-1));
     return cm;
 }
-/*
-vector<int> v = {3,1,5,2,1};
-int n = v.size();
 
-vector<char> pd(int i, int w, vector<char> acum, int res){
-    if(i==n){
-        if(res==w){
-            return acum;
-        }else{
-            return {};
-        }
-    }
-
-    vector<char> s(acum);
-    s.push_back('s');
-
-    vector<char> suma = pd(i+1,w,s,res+v[i]);
-    if(!suma.empty()){
-        return suma;
-    }
-
-    vector<char> m(acum);
-    m.push_back('m');
-
-    vector<char> prod = pd(i+1,w,m,res*v[i]);
-    if(!prod.empty()){
-        return prod;
-    }
-
-    vector<char> p(acum);
-    p.push_back('p');
-    vector<char> pot = pd(i+1,w,p,pow(res,v[i]));
-    if(!pot.empty()){
-        return pot;
-    }
-}
-*/
 int main() {
     std::cout << "Cuadrado magico:" << std::endl;
-    /*
-    vector<char> res = pd(1,400,{},v[0]);
-    for(auto elem : res){
-        cout << elem;
-    }
-     */
+    
     int n=3;
     vector<vector<int>> cuadrado(n,vector<int>(n));
     vector<int> numeros(n*n,0);
