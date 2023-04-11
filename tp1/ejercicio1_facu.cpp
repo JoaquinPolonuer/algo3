796df37b4285e177fe2aa481540d19028c79fa7b
facundo2gonzalez2 <facundo2gonzalez2@gmail.com>
2023-04-11 12:40:06 -0300

    Ej1 completo e intento de demo del ej3

--- tp1/ejercicio1_facu.cpp
+++ tp1/ejercicio1_facu.cpp
@@ -12,14 +12,15 @@ vector<int> sumasDiag;
 vector<bool> numeros;
 int m;
 
+vector<vector<int>> matriz;
 
-void imprimir_matriz()
+void imprimir_matriz(vector<vector<int>> M)
 {
-    for (int fila = 0; fila < n; fila++)
+    for (int fila = 0; fila < M.size(); fila++)
     {
-        for (int columna = 0; columna < n; columna++)
+        for (int columna = 0; columna < M.size(); columna++)
         {
-            cout << cm[fila][columna] << " ";
+            cout << M[fila][columna] << " ";
         }
         cout << endl;
     }
@@ -34,41 +35,29 @@ void cuadradoMagicoKesimo(int i, int j){
     if(i==n){
         k--;
         if(k==0){
-            imprimir_matriz();
+            for(int i=0;i<cm.size();i++){
+                for(int j=0;j<cm.size();j++){
+                    matriz[i][j]=cm[i][j];
+                }
+            }
         }
     }
 
     for (int l = 1; l <= n * n; l++) {
         bool valido = true;
-        if (numeros[l - 1] == 1){
-            valido = false;
-        }
-        if (sumasFil[i] + l > m){
-            valido = false;
-        }
-        if (j == n - 1 && sumasFil[i] + l != m) {
-            valido = false;
-        }
-        if (sumasCol[j] + l > m){
-            valido = false;
-        }
-        if (i == n - 1 && sumasCol[j] + l != m){
-            valido = false;
-        }
-
-        if (i==j && sumasDiag[0] + l > m){
-            valido = false;
-        }
-
-        if (i==j && i == n - 1 && sumasDiag[0] + l != m){
-            valido = false;
-        }
-
-        if (i==n-j-1 && sumasDiag[1] + l > m) {
-            valido = false;
-        }
-
-        if (i==n-j-1 && i == n - 1 && sumasDiag[1] + l != m) {
+        if (                            //podas:
+            (numeros[l - 1] == 1) or
+            (sumasFil[i] + l > m) or
+            (j == n - 1 && sumasFil[i] + l != m) or
+            (sumasCol[j] + l > m) or
+            (i == n - 1 && sumasCol[j] + l != m) or
+            (i==j && sumasDiag[0] + l > m) or
+            (i==j && i == n - 1 && sumasDiag[0] + l != m) or
+            (i==n-j-1 && sumasDiag[1] + l > m) or
+            (i==n-j-1 && i == n - 1 && sumasDiag[1] + l != m) or
+            (k<0)
+            ) 
+        {
             valido = false;
         }
 
@@ -96,10 +85,9 @@ void cuadradoMagicoKesimo(int i, int j){
 }
 
 int main() {
-    cout << "Cuadrado magico:" << endl;
-    
     cin >> n >> k;
     cm.resize(n,vector<int>(n,0));
+    matriz.resize(n,vector<int>(n,-1));
     sumasDiag.resize(2, 0);
     sumasFil.resize(n, 0);
     sumasCol.resize(n, 0);
@@ -108,7 +96,11 @@ int main() {
 
     cuadradoMagicoKesimo(0,0);
 
-    
+    if(matriz[0][0]==-1){
+        cout << "-1" << endl;
+    }else{
+        imprimir_matriz(matriz);
+    }
     return 0;
 }
 
