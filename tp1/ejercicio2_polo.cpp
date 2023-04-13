04d37793ce6b325fdc0ec48519099557d6c65575
joaquinpolonuer <jtpolonuer@gmail.com>
2023-04-13 12:39:17 -0300

    correcciones ej 2

--- tp1/ejercicio2_polo.cpp
+++ tp1/ejercicio2_polo.cpp
@@ -4,20 +4,20 @@
 
 using namespace std;
 
-int c;
-int n, r, m;
-vector<int> v;
-vector<vector<int>> dp;
+long long int c;
+long long int n, r, m;
+vector<long long int> v;
+vector<vector<long long int>> dp;
 
-int potencia_modular(int a, int k)
+long long int potencia_modular(long long int a, long long int k)
 {
     if (k == 0)
     {
-        return a;
+        return 1;
     }
     else
     {
-        int aux = potencia_modular(a, k / 2) % m;
+        long long int aux = potencia_modular(a, k / 2) % m;
         if (k % 2 == 0)
         {
             return (aux * aux) % m;
@@ -29,33 +29,35 @@ int potencia_modular(int a, int k)
     }
 }
 
-int modulo_positivo(int a)
+long long int modulo_positivo(long long int a)
 {
     return (m + (a % m)) % m;
 }
 
-bool operaciones(long long int mod_acum, long int i)
+bool operaciones(long long int mod_acum, long long int i)
 {
     if (dp[mod_acum][i] != -1)
     {
         return dp[mod_acum][i] == 1;
     }
 
-    bool res;
 
+    bool res;
     if (i == n)
     {
         res = mod_acum == r;
     }
     else
     {
-        bool suma = operaciones(modulo_positivo(mod_acum + v[i]), i + 1);
-        bool resta = operaciones(modulo_positivo(mod_acum - v[i]), i + 1);
-        bool multi = operaciones(modulo_positivo(mod_acum * v[i]), i + 1);
+        long long int mod_v = modulo_positivo(v[i]);
 
-        int p = potencia_modular(mod_acum, v[i]);
+        bool suma = operaciones(modulo_positivo(mod_acum + mod_v), i + 1);
+        bool resta = operaciones(modulo_positivo(mod_acum - mod_v), i + 1);
+        bool multi = operaciones(modulo_positivo(mod_acum * mod_v), i + 1);
+
+        long long int p = potencia_modular(mod_acum, mod_v);
         bool potencia = operaciones(modulo_positivo(p), i + 1);
-        bool res = suma or resta or multi or potencia;
+        res = (suma or resta or multi or potencia);
     }
 
     if (res)
@@ -75,17 +77,17 @@ int main()
     cin >> c;
 
     // Esta bien como manejo esta entrada?
-    for (int i = 0; i < c; i++)
+    for (long long int i = 0; i < c; i++)
     {
         cin >> n >> r >> m;
-        v = vector<int>(); 
-        for (int j = 0; j < n; j++)
+        v = vector<long long int>(); 
+        for (long long int j = 0; j < n; j++)
         {
-            int x;
+            long long int x;
             cin >> x;
             v.push_back(x);
         }
-        dp = vector<vector<int>>(m, vector<int>(n + 1, -1));
+        dp = vector<vector<long long int>>(m, vector<long long int>(n + 1, -1));
         
         bool res = operaciones(modulo_positivo(v[0]), 1);
         if (res)
--- tp1/ejercicio3_complejidad.cpp
+++ tp1/ejercicio3_complejidad.cpp
@@ -10,9 +10,10 @@
 using namespace std;
 
 int n;
-vector<tuple<int,int,int>> A;
+vector<tuple<int, int, int>> A;
 
-void merge(vector<tuple<int, int, int>>& arr, int left, int middle, int right) {
+void merge(vector<tuple<int, int, int>> &arr, int left, int middle, int right)
+{
     int n1 = middle - left + 1;
     int n2 = right - middle;
     vector<tuple<int, int, int>> L(n1), R(n2);
@@ -21,30 +22,37 @@ void merge(vector<tuple<int, int, int>>& arr, int left, int middle, int right) {
     for (int j = 0; j < n2; j++)
         R[j] = arr[middle + 1 + j];
     int i = 0, j = 0, k = left;
-    while (i < n1 && j < n2) {
-        if (get<2>(L[i]) <= get<2>(R[j])) {
+    while (i < n1 && j < n2)
+    {
+        if (get<2>(L[i]) <= get<2>(R[j]))
+        {
             arr[k] = L[i];
             i++;
         }
-        else {
+        else
+        {
             arr[k] = R[j];
             j++;
         }
         k++;
     }
-    while (i < n1) {
+    while (i < n1)
+    {
         arr[k] = L[i];
         i++;
         k++;
     }
-    while (j < n2) {
+    while (j < n2)
+    {
         arr[k] = R[j];
         j++;
         k++;
     }
 }
-void mergeSort(vector<tuple<int, int, int>>& arr, int left, int right) {
-    if (left < right) {
+void mergeSort(vector<tuple<int, int, int>> &arr, int left, int right)
+{
+    if (left < right)
+    {
         int middle = left + (right - left) / 2;
         mergeSort(arr, left, middle);
         mergeSort(arr, middle + 1, right);
@@ -52,62 +60,69 @@ void mergeSort(vector<tuple<int, int, int>>& arr, int left, int right) {
     }
 }
 
-void actividades(){
-	mergeSort(A,0,A.size()-1);  //O(n logn) --> mergeSort
-	int ultAct = 0;
+void actividades()
+{
+    mergeSort(A, 0, A.size() - 1); // O(n logn) --> mergeSort
+    int ultAct = 0;
     vector<int> S;
-    for(int i = 0; i < A.size(); i++){
-        if (get<1>(A[i]) >= ultAct){
-            S.push_back(get<0>(A[i])+1);
+    for (int i = 0; i < A.size(); i++)
+    {
+        if (get<1>(A[i]) >= ultAct)
+        {
+            S.push_back(get<0>(A[i]) + 1);
             ultAct = get<2>(A[i]);
         }
     }
 
-    cout << S.size();  //Para ir viendo el tamaño de la solución con cada N
-    cout << endl;
+    // cout << S.size(); // Para ir viendo el tamaño de la solución con cada N
+    // cout << endl;
 }
 
-vector<tuple<int,int,int>> construir_vector(int n){
+vector<tuple<int, int, int>> construir_vector(int n)
+{
 
-    vector<tuple<int,int,int>> res;
-    srand (time(NULL));
-    tuple<int,int,int> numero;
+    vector<tuple<int, int, int>> res;
+    srand(time(NULL));
+    tuple<int, int, int> numero;
 
-    for(int i=0; i < n; i++){
-        int fin_act = rand() % (2*n) ;
-        int inic_act = rand() % 2*n;
-        numero = make_tuple(i,inic_act,fin_act);
+    for (int i = 0; i < n; i++)
+    {
+        int fin_act = rand() % (2 * n);
+        int inic_act = rand() % (fin_act + 1);
+        numero = make_tuple(i, inic_act, fin_act);
         res.push_back(numero);
     }
     return res;
 }
-void complejidad(){
-    int n=1;
-    int hasta = 50000;
+void complejidad()
+{
+    int n = 1;
+    int hasta = 1000000;
     int paso = 500;
     ofstream fout;
     fout.open("datos_ej3.csv");
-    fout << "n\t" << "tiempo" <<endl;
-    while (n<hasta){
-        vector<tuple<int,int,int>> v= construir_vector(n);
-
-        double t0=clock();
-        A = v;
+    fout << "n\t"
+         << "tiempo" << endl;
+    while (n < hasta)
+    {
+        A = construir_vector(n);
+        
+        double t0 = clock();
         actividades();
-        double t1=clock();
+        double t1 = clock();
 
-        double tiempo = (double (t1-t0) / CLOCKS_PER_SEC);
+        double tiempo = (double(t1 - t0) / CLOCKS_PER_SEC);
 
         fout << n << "\t" << tiempo << endl;
 
         n += paso;
     }
     fout.close();
-
 }
 
-int main(){
+int main()
+{
     complejidad();
-	
-	return 0;
+
+    return 0;
 }
