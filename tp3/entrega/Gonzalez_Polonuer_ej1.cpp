0ff7f6d598c4b3fc1ec68b6119bd90384081ac05
joaquinpolonuer <jtpolonuer@gmail.com>
2023-06-19 15:10:40 -0300

    carpeta

---
+++ tp3/entrega/Gonzalez_Polonuer_ej1.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <tuple>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+using ll = long long;
+const ll inf = 1e18;
+
+int C;
+ll N, M, K, s, t;
+ll u, v, c;
+
+vector<vector<pair<ll, ll>>> g_aristas;
+vector<vector<pair<ll, ll>>> gt_aristas;
+vector<tuple<ll, ll, ll>> aristas_extra;
+
+vector<ll> dijkstra(vector<vector<pair<ll, ll>>> &g, ll s)
+{
+    vector<ll> d = vector<ll>(N, inf);
+    vector<bool> processed = vector<bool>(N, false);
+
+    queue<pair<ll, ll>> q;
+
+    d[s] = 0;
+    q.push({0, s});
+
+    while (!q.empty())
+    {
+        ll u = q.front().second;
+        q.pop();
+
+        if (processed[u])
+            continue;
+
+        processed[u] = true;
+        for (auto e : g[u])
+        {
+            ll v = e.first;
+            ll c = e.second;
+
+            if (d[u] + c < d[v])
+            {
+                d[v] = d[u] + c;
+                q.push({-d[v], v});
+            }
+        }
+    }
+
+    return d;
+}
+
+int main()
+{
+
+    cin >> C;
+
+    // Itero por la cantidad de casos de prueba
+    for (int caso = 0; caso < C; caso++)
+    {
+        // recibo el input
+        // para no tener problema con el indexado, voy a
+        // restarle 1 a todos los vertices, de manera que
+        // vayan de 0 a n-1
+        cin >> N >> M >> K >> s >> t;
+        s--, t--;
+
+        g_aristas = vector<vector<pair<ll, ll>>>(N);
+        gt_aristas = vector<vector<pair<ll, ll>>>(N);
+        aristas_extra = vector<tuple<ll, ll, ll>>();
+
+        // lleno mis listas de adyacencias para g y gt
+        for (int i = 0; i < M; i++)
+        {
+            cin >> u >> v >> c;
+            u--;
+            v--;
+            g_aristas[u].push_back({v, c});
+            gt_aristas[v].push_back({u, c});
+        }
+
+        // guardo las aristas extra que puedo agregar
+        for (int i = 0; i < K; i++)
+        {
+            cin >> u >> v >> c;
+            u--;
+            v--;
+            aristas_extra.push_back({u, v, c});
+        }
+
+        // para cada nodo u calculo
+        // d(s, u) y d(u, t)
+        vector<ll> d_d_s = dijkstra(g_aristas, s);
+        vector<ll> d_a_t = dijkstra(gt_aristas, t);
+
+        // el camino minimo antes de hacer cualquier
+        // cambio es el que encuentra dijkstra
+        ll cm = d_d_s[t];
+        for (auto e : aristas_extra)
+        {
+            tie(u, v, c) = e;
+
+            // si la distancia a t disminuye al usar mi nueva
+            // arista e, cambio la longitud del camino minimo
+            if (d_d_s[u] + c + d_a_t[v] < cm)
+            {
+                cm = d_d_s[u] + c + d_a_t[v];
+            }
+
+            if (d_d_s[v] + c + d_a_t[u] < cm)
+            {
+                cm = d_d_s[v] + c + d_a_t[u];
+            }
+        }
+        if (cm != inf)
+        {
+            cout << cm << endl;
+        }
+        else
+        {
+            cout << -1 << endl;
+        }
+    }
+
+    return 0;
+}
---
+++ tp3/entrega/Gonzalez_Polonuer_ej2.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <tuple>
+#include <algorithm>
+#include <queue>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+const ll inf = 1e18;
+
+ll N, M;
+
+ll voto;
+vector<vector<ll>> vecinos;
+vector<vector<ll>> capacidad_residual;
+ll i, j;
+
+ll bfs(ll s, ll t, vector<ll>& parent) {
+    fill(parent.begin(), parent.end(), -1);
+    parent[s] = -2;
+    queue<pair<ll, ll>> q;
+    q.push({s, inf});
+
+    while (!q.empty()) {
+        ll cur = q.front().first;
+        ll flow = q.front().second;
+        q.pop();
+
+        for (ll next : vecinos[cur]) {
+            if (parent[next] == -1 && capacidad_residual[cur][next]) {
+                parent[next] = cur;
+                ll delta_p = min(flow, capacidad_residual[cur][next]);
+                if (next == t)
+                    return delta_p;
+                q.push({next, delta_p});
+            }
+        }
+    }
+
+    return 0;
+}
+
+// Implementacion de https://cp-algorithms.com/graph/edmonds_karp.html#implementation
+ll flujo_maximo(ll s, ll t) {
+    ll flow = 0;
+    vector<ll> parent(N+2);
+    ll delta_p;
+
+    while (delta_p = bfs(s, t, parent)) {
+        flow += delta_p;
+        ll cur = t;
+        while (cur != s) {
+            ll prev = parent[cur];
+            capacidad_residual[prev][cur] -= delta_p;
+            capacidad_residual[cur][prev] += delta_p;
+            cur = prev;
+        }
+    }
+
+    return flow;
+}
+
+
+int main()
+{
+    // S = 0, T = N+1
+    while (true)
+    {
+        scanf("%lld %lld", &N, &M);
+
+        if (N == 0 && M == 0)
+        {
+            break;
+        }
+
+        // Vamos a crear la lista de nodos adyacentes para cada nodo.
+        // Consideramos que S=0 y T=N+1.
+        // Ademas, creamos una matriz que indica la capacidad de una
+        // arista en la red residual, lo hacemos de esta manera porque
+        // es compatible con la implementacion usada de E&K.
+        vecinos = vector<vector<ll>>(N + 2);
+        capacidad_residual = vector<vector<ll>>(N + 2, vector<ll>(N + 2, 0));
+
+        for (ll i = 1; i <= N; i++)
+        {
+            scanf("%lld",&voto);
+
+            ll conexion;
+            if (voto == 0)
+            {
+                conexion = 0;
+            }
+            else
+            {
+                conexion = N + 1;
+            }
+
+            // Conectamos los que votan 0 a s y los que votan
+            // 1 a t, siempre con capacidad 1.
+            vecinos[conexion].push_back(i);
+            vecinos[i].push_back(conexion);
+
+            capacidad_residual[conexion][i] = 1;
+            capacidad_residual[i][conexion] = 1;
+        }
+
+        for (ll k = 0; k < M; k++)
+        {
+            scanf("%lld %lld", &i, &j);
+
+            // Conectamos los amigos entre sí.
+            vecinos[i].push_back(j);
+            vecinos[j].push_back(i);
+
+            capacidad_residual[i][j] = 1;
+            capacidad_residual[j][i] = 1;
+        }
+
+        // La cantidad minima de disconformidades va a ser igual
+        // al valor de corte minimo, que es igual al flujo maximo.
+        cout << flujo_maximo(0, N + 1) << endl;
+    }
+
+    return 0;
+}
